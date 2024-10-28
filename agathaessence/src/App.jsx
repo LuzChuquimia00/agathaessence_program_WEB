db79dcd2cf044a0c2d14020ba0ca130365b0f6b6
LuzChuquimia <luzchuquimia1680@gmail.com>
2024-10-28 17:06:41 -0300

    agregue una ruta de inicio de sesion q esta en proceso...

--- agathaessence/src/App.jsx
+++ agathaessence/src/App.jsx
@@ -1,11 +1,12 @@
 import SlidingNavigationBar from "./components/SlidingTabBar";
 import FooterBar from "./components/FooterBar";
-
+import LoginForm from "./pagina/Assets/LoginForm/LoginForm";
 export function App() {
   return (
     <>
       <SlidingNavigationBar />
       <FooterBar />
+      <LoginForm />
     </>
   );
 }
